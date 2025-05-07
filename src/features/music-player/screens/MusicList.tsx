231701f89f07b19d9f53cdd3512a7d278771b168
Esteban2002Dev <tsuesteban2002@gmail.com>
2025-05-07 02:02:51 -0600

    Add(Music player): shared header component with dynamic props
    Add declarations file to manage images as modules

--- src/features/music-player/screens/MusicList.tsx
+++ src/features/music-player/screens/MusicList.tsx
@@ -1,14 +1,37 @@
+import { Header } from '@features/music-player/components/Header';
 import { globalStyles } from '@config/theme/GlobalStyles.styles';
 import { Appbar } from '@shared/components/Appbar';
-import { View, Text } from 'react-native';
+import { COLORS } from '@config/theme/Colors';
+import { Image, View } from 'react-native';
 import React from 'react';
+
+/**
+ * * To import images as modules we have to create the declarations file (declarations.d.ts)
+ * * and declare the file type as module.
+ */
+import profileImg from '@assets/images/esteban.jpeg';
 
 export function MusicList() {
     return (
         <View style={[globalStyles.container, globalStyles.background]}>
             <Appbar showBackButton />
             <View style={globalStyles.appContainer}>
-                <Text>MusicList</Text>
+                <Header
+                title='Good Morning'
+                subtitle='Luis Esteban!'
+                description='Listen to your music.'
+                rightContent={
+                    // TODO: Make this a shared component (ex. <Avatar />)
+                    <View>
+                        <Image source={profileImg} style={{
+                            width: 100,
+                            height: 100,
+                            borderRadius: 50,
+                            borderWidth: 2,
+                            borderColor: COLORS.PURPLE.base
+                            }} />
+                    </View>
+                } />
             </View>
         </View>
     )
