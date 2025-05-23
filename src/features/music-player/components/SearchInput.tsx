0718ee326cb6ed38b5ef01cc1b57b714aee05360
Esteban2002Dev <tsuesteban2002@gmail.com>
2025-05-22 22:44:44 -0600

    Add(theme): coral color
    Fix shadow for search input

--- src/features/music-player/components/SearchInput.tsx
+++ src/features/music-player/components/SearchInput.tsx
@@ -67,7 +67,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.1,
         shadowRadius: 4,
 
-        elevation: 10,
+        elevation: 3,
     },
     icon: {
         marginRight: 8,
